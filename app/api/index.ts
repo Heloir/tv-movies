114f7009132057e048223507e931d0eb777e5a9c
Pedro Vitor <80995107+Heloir@users.noreply.github.com>
2023-06-03 19:06:08 -0300

    fix: api baseURL

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api-auth-two.vercel.app',
+    baseURL: 'https://api-auth-sigma.vercel.app',
 });
 export default api;
