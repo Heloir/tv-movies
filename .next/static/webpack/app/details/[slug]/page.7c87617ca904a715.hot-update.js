091fc86e0c54a9bc31e75908be7fdc0d7d59d8f5
Pedro Vitor <80995107+Heloir@users.noreply.github.com>
2023-06-04 21:00:10 -0300

    fix: .gitignore

---
+++ .next/static/webpack/app/details/[slug]/page.7c87617ca904a715.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/details/[slug]/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CMovieDecription.tsx&modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CActorsComponente.tsx&server=false!":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CMovieDecription.tsx&modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CActorsComponente.tsx&server=false! ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/details/components/MovieDecription.tsx */ \"(app-client)/./app/details/components/MovieDecription.tsx\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/details/components/ActorsComponente.tsx */ \"(app-client)/./app/details/components/ActorsComponente.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUMlM0ElNUNVc2VycyU1Q0hvcGUlNUNEZXNrdG9wJTVDZmlsbXMlNUNXZWIlNUNhcHAlNUNkZXRhaWxzJTVDY29tcG9uZW50cyU1Q01vdmllRGVjcmlwdGlvbi50c3gmbW9kdWxlcz1DJTNBJTVDVXNlcnMlNUNIb3BlJTVDRGVza3RvcCU1Q2ZpbG1zJTVDV2ViJTVDYXBwJTVDZGV0YWlscyU1Q2NvbXBvbmVudHMlNUNBY3RvcnNDb21wb25lbnRlLnRzeCZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLDJNQUF3SDtBQUN4SCIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzU2NzUiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJDOlxcXFxVc2Vyc1xcXFxIb3BlXFxcXERlc2t0b3BcXFxcZmlsbXNcXFxcV2ViXFxcXGFwcFxcXFxkZXRhaWxzXFxcXGNvbXBvbmVudHNcXFxcTW92aWVEZWNyaXB0aW9uLnRzeFwiKTtcbmltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxcSG9wZVxcXFxEZXNrdG9wXFxcXGZpbG1zXFxcXFdlYlxcXFxhcHBcXFxcZGV0YWlsc1xcXFxjb21wb25lbnRzXFxcXEFjdG9yc0NvbXBvbmVudGUudHN4XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CMovieDecription.tsx&modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CActorsComponente.tsx&server=false!\n"));
+
+/***/ })
+
+});
---
+++ .next/static/webpack/app/details/[slug]/page.dbe8e99576a3e16b.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/details/[slug]/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CActorsComponente.tsx&modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CMovieDecription.tsx&server=false!":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CActorsComponente.tsx&modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CMovieDecription.tsx&server=false! ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/details/components/ActorsComponente.tsx */ \"(app-client)/./app/details/components/ActorsComponente.tsx\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/details/components/MovieDecription.tsx */ \"(app-client)/./app/details/components/MovieDecription.tsx\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUMlM0ElNUNVc2VycyU1Q0hvcGUlNUNEZXNrdG9wJTVDZmlsbXMlNUNXZWIlNUNhcHAlNUNkZXRhaWxzJTVDY29tcG9uZW50cyU1Q0FjdG9yc0NvbXBvbmVudGUudHN4Jm1vZHVsZXM9QyUzQSU1Q1VzZXJzJTVDSG9wZSU1Q0Rlc2t0b3AlNUNmaWxtcyU1Q1dlYiU1Q2FwcCU1Q2RldGFpbHMlNUNjb21wb25lbnRzJTVDTW92aWVEZWNyaXB0aW9uLnRzeCZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLDZNQUF5SDtBQUN6SCIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzJlMmUiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJDOlxcXFxVc2Vyc1xcXFxIb3BlXFxcXERlc2t0b3BcXFxcZmlsbXNcXFxcV2ViXFxcXGFwcFxcXFxkZXRhaWxzXFxcXGNvbXBvbmVudHNcXFxcQWN0b3JzQ29tcG9uZW50ZS50c3hcIik7XG5pbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkM6XFxcXFVzZXJzXFxcXEhvcGVcXFxcRGVza3RvcFxcXFxmaWxtc1xcXFxXZWJcXFxcYXBwXFxcXGRldGFpbHNcXFxcY29tcG9uZW50c1xcXFxNb3ZpZURlY3JpcHRpb24udHN4XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CActorsComponente.tsx&modules=C%3A%5CUsers%5CHope%5CDesktop%5Cfilms%5CWeb%5Capp%5Cdetails%5Ccomponents%5CMovieDecription.tsx&server=false!\n"));
+
+/***/ })
+
+});
